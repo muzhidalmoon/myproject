92582a418ece0a1a1433db8ef609342a0d1ca0a4
muzhidalmoon <muzahid.ba3@gmail.com>
2023-04-25 09:30:34 +0600

    files is added

---
+++ dist/web.js
@@ -0,0 +1,31 @@
+    document.getElementsByTagName('button')[0].addEventListener('click',()=>{
+        document.getElementById('nav-links').classList.toggle('active')
+    })
+
+
+   document.getElementById("seo").addEventListener("click", () => {
+     document.getElementById("boxes").classList.toggle("run");
+     document.getElementById("seo").classList.toggle("bgBlue");
+   });
+//    document.getElementById("cc").addEventListener("click", () => {
+//      document.getElementById("boxes").classList.toggle("run");
+//      document.getElementById("cc").classList.toggle("bgBlue");
+//    });
+//    document.getElementById("sem").addEventListener("click", () => {
+//      document.getElementById("boxes").classList.toggle("run");
+//      document.getElementById("sem").classList.toggle("bgBlue");
+//    });
+//    document.getElementById("cd").addEventListener("click", () => {
+//      document.getElementById("boxes").classList.toggle("run");
+//      document.getElementById("cd").classList.toggle("bgBlue");
+//    });
+//    document.getElementById("smm").addEventListener("click", () => {
+//      document.getElementById("boxes").classList.toggle("run");
+//      document.getElementById("smm").classList.toggle("bgBlue");
+//    });
+//    document.getElementById("em").addEventListener("click", () => {
+//      document.getElementById("boxes").classList.toggle("run");
+//      document.getElementById("seo").classList.toggle("bgBlue");
+//    });
+ 
+    
